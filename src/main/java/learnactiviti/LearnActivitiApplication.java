ac99939bf97a142704839fbad4e73e8b83fed03e
baihezhuo <174754613@qq.com>
2017-09-21 17:11:12 +0800

    init

---
+++ src/main/java/learnactiviti/LearnActivitiApplication.java
@@ -0,0 +1,12 @@
+package learnactiviti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnActivitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnActivitiApplication.class, args);
+	}
+}
---
+++ src/test/java/learnactiviti/LearnActivitiApplicationTests.java
@@ -0,0 +1,16 @@
+package learnactiviti;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnActivitiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
