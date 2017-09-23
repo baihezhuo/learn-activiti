7064a254e20d08d81ea44c960420f49d36b5c910
baihezhuo <174754613@qq.com>
2017-09-23 13:58:44 +0800

    add test

--- src/test/java/learnactiviti/LearnActivitiApplicationTests.java
+++ src/test/java/learnactiviti/LearnActivitiApplicationTests.java
@@ -1,16 +1,22 @@
 package learnactiviti;
 
+import org.activiti.engine.RepositoryService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = LearnActivitiApplication.class)
 public class LearnActivitiApplicationTests {
 
+	@Autowired
+	private RepositoryService repositoryService;
+	
 	@Test
 	public void contextLoads() {
+		System.err.println(repositoryService);
 	}
 
 }
